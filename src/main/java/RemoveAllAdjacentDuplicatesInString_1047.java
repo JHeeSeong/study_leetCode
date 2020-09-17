b169ce30f9ec0b556b78062b6b833a72e8f9a194
heeseong <hsung0823@ipageon.com>
2020-09-17 16:31:03 +0900

    1047. Remove All Adjacent Duplicates In String

---
+++ src/main/java/RemoveAllAdjacentDuplicatesInString_1047.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class RemoveAllAdjacentDuplicatesInString_1047 {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.removeDuplicates("abbaca"));
+    }
+
+    static class Solution {
+        public String removeDuplicates(String S) {
+            char[] stringArr = S.toCharArray();
+
+
+            List<String> myList = new ArrayList<>();
+            for (char c : stringArr)
+                myList.add(c + "");
+
+            int i = 0;
+            while (i < myList.size() - 1) {
+                if (myList.get(i).equals(myList.get(i + 1))) {
+                    myList.remove(i + 1);
+                    myList.remove(i);
+                    i = 0;
+                    continue;
+                }
+                i++;
+            }
+
+            String result = "";
+            for (String s : myList)
+                result += s;
+            return result;
+        }
+    }
+}
