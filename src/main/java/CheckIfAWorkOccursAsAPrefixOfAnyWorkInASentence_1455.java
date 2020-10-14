f4f55fd6c21e14104fba6eb914aa9d78a151b16f
heeseong <hsung0823@ipageon.com>
2020-10-14 09:00:06 +0900

    1455. Check If A Work Occurs As A Prefix Of Any Work In A Sentence

---
+++ src/main/java/CheckIfAWorkOccursAsAPrefixOfAnyWorkInASentence_1455.java
@@ -0,0 +1,26 @@
+public class CheckIfAWorkOccursAsAPrefixOfAnyWorkInASentence_1455 {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+//        System.out.println(solution.isPrefixOfWord("i love eating burger", "burg"));
+//        System.out.println(solution.isPrefixOfWord("this problem is an easy problem", "pro"));
+//        System.out.println(solution.isPrefixOfWord("i am tired", "you"));
+//        System.out.println(solution.isPrefixOfWord( "i use triple pillow", "pill"));
+//        System.out.println(solution.isPrefixOfWord("hello from the other side", "they"));
+
+        System.out.println(solution.isPrefixOfWord("hellohello hellohellohello", "ell"));
+    }
+
+    static class Solution {
+        public int isPrefixOfWord(String sentence, String searchWord) {
+            int result = -1;
+            String[] sentences = sentence.split(" ");
+            for (int i = 0; i < sentences.length; i++) {
+                if (sentences[i].startsWith(searchWord) ) {
+                    return i + 1;
+                }
+            }
+
+            return result;
+        }
+    }
+}
