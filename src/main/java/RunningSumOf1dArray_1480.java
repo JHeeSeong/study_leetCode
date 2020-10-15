9306c9a5c9fad0984042acba992f13ee60deefee
heeseong <hsung0823@ipageon.com>
2020-10-15 09:10:30 +0900

    1480. Running Sum Of 1d Array

--- src/main/java/RunningSumOf1dArray_1480.java
+++ src/main/java/RunningSumOf1dArray_1480.java
@@ -1,23 +1,28 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class RunningSumOf1dArray_1480 {
     public static void main(String[] args) {
         Solution solution = new Solution();
-//        int[] nums = new int[]{1, 2, 3, 4};
-//        int[] nums = new int[]{1, 1, 1, 1, 1};
-        int[] nums = new int[]{3, 1, 2, 10, 1};
-        solution.runningSum(nums);
+//        int[] arr = new int[]{1, 2, 3, 4};
+//        int[] arr = new int[]{1, 1, 1, 1, 1};
+        int[] arr = new int[]{3, 1, 2, 10, 1};
+        System.out.println(solution.runningSum(arr));
     }
 
     static class Solution {
         public int[] runningSum(int[] nums) {
-            int[] result = new int[nums.length];
+            List<Integer> list = Arrays.stream(nums).boxed().collect(Collectors.toList());
+            List<Integer> result = new ArrayList<>();
 
-            int sum = 0;
-            for (int i = 0; i < nums.length; i++) {
-                sum += nums[i];
-                result[i] = sum;
+            result.add(list.get(0));
+            for (int i = 1; i < list.size(); i++) {
+                int sum = list.get(i) + result.get(i - 1);
+                result.add(sum);
             }
-
-            return result;
+            return result.stream().mapToInt(i -> i).toArray();
         }
     }
 }
