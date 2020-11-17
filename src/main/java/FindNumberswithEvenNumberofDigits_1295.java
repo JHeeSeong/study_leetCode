6c18d74fe93f71a73530302da938c75a71b0145a
heeseong <hsung0823@ipageon.com>
2020-11-17 16:13:31 +0900

    1295. Find Numbers with Even Number of Digits

---
+++ src/main/java/FindNumberswithEvenNumberofDigits_1295.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class FindNumberswithEvenNumberofDigits_1295 {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+//        int[] arr = new int[]{12, 345, 2, 6, 7896};
+        int[] arr = new int[]{555,901,482,1771};
+
+        System.out.println(solution.findNumbers(arr));
+    }
+
+    static class Solution {
+        public int findNumbers(int[] nums) {
+            int count = 0;
+            for (int i = 0; i < nums.length; i++) {
+                int length = (nums[i] + "").length();
+                if (length % 2 == 0)
+                    count++;
+            }
+
+            return count;
+        }
+    }
+}
