cc1692f2d11e6966406914545e3a70f3a18ee44e
heeseong <hsung0823@ipageon.com>
2020-11-19 10:34:20 +0900

    709. To Lower Case

---
+++ src/main/java/ToLowerCase_709.java
@@ -0,0 +1,13 @@
+public class ToLowerCase_709 {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        solution.toLowerCase("Hello");
+    }
+
+    static class Solution {
+        public String toLowerCase(String str) {
+            return str.toLowerCase();
+        }
+    }
+}
