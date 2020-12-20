c503439346f5c80a8d2eaad8f991ab0e0794c8c3
heeseong <hsung0823@ipageon.com>
2020-12-20 20:32:03 +0900

    961. N-Repeated Element in Size 2N Array

---
+++ src/main/java/NRepeatedElementinSize2NArray_961.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class NRepeatedElementinSize2NArray_961 {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] arr = new int[]{1, 2, 3, 3};
+        System.out.println(solution.repeatedNTimes(arr));
+    }
+
+    static class Solution {
+        public int repeatedNTimes(int[] A) {
+            List<Integer> list = Arrays.stream(A).boxed().collect(Collectors.toList());
+            int result = 0;
+            int value = 0;
+            HashMap<Integer, Integer> hashMap = new HashMap();
+            for (int i = 0; i < list.size(); i++) {
+                hashMap.put(A[i], 0);
+            }
+
+            for (int i = 0; i < list.size(); i++) {
+                if (list.contains(A[i])) {
+                    int temp = hashMap.get(A[i]);
+                    hashMap.put(A[i], temp += 1);
+                }
+            }
+
+            for (Map.Entry<Integer, Integer> entry : hashMap.entrySet()) {
+                if (result < entry.getValue()) {
+                    result = entry.getValue();
+                    value = entry.getKey();
+                }
+            }
+
+            return value;
+        }
+    }
+}
+
